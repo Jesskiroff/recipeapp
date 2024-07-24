e1612151bad670dbbcd85f2ecd01b4428772439f
Jessica Kiroff <jesskiroff@gmail.com>
2024-07-24 16:30:44 -0400

    initialize api call

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,10 +1,13 @@
 
 function Popular() {
-  return (
-    <div>
-      Popular
-    </div>
-  )
-}
+    const getPopular = async () => {
+        const api = await fetch(
+            `https://api.spoonacular.com/recipes/random=${process.env.REACT_APP_API_KEY}&number=15`
+        );
+        const data = await api.json();
+        console.log(data);
+    };
+    return <div>Popular</div>;
+    }
 
-export default Popular
+export default Popular;
