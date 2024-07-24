c7432d42335de6b12545ceb5a21cabbc19c1d93a
Jessica Kiroff <jesskiroff@gmail.com>
2024-07-24 17:09:49 -0400

    fix api key error

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
 
 function Popular() {
+
+    useEffect(() => {
+        getPopular();
+    }, []); //An empty dependency array 
+            //means that the effect does not depend on any values from the component scope. 
+            //Therefore, it should only run once after the initial render and not on any subsequent renders.
     const getPopular = async () => {
         const api = await fetch(
-            `https://api.spoonacular.com/recipes/random=${process.env.REACT_APP_API_KEY}&number=15`
+            `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
         );
         const data = await api.json();
         console.log(data);
